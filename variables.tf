5c5c15dfb1fc3f8ee2095fbd7d0e76c2b0afe720
root <root@ip-172-31-38-213.ec2.internal>
2023-12-07 23:17:19 +0000

    chore: init infra

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable vpc_id {
+  type        = string
+  description = "vpc-07cd5506f7d918e8d"
+}
+
+variable subnet_id {
+  type        = string
+  description = "subnet-0126234655070743b"
+}
+
